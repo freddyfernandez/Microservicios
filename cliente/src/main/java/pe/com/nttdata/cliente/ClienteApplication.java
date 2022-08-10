67395db5a6917585de1f3e4778e8e906443e19f0
freddy fernandez <fernandezyc@hotmail.com>
2022-08-10 00:36:22 -0500

    minikube deploy

--- cliente/src/main/java/pe/com/nttdata/cliente/ClienteApplication.java
+++ cliente/src/main/java/pe/com/nttdata/cliente/ClienteApplication.java
@@ -2,6 +2,8 @@ package pe.com.nttdata.cliente;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -13,6 +15,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
         basePackages = "pe.com.nttdata.clientefeign" //para comunicar con el modulo clientefeign del paquete clientefeign
 
 )
+@PropertySources({
+        @PropertySource("classpath:clientefeign-${spring.profiles.active}.properties")
+})
 public class ClienteApplication {
 
     public static void main(String[] args){
--- clientefeign/src/main/java/pe/com/nttdata/clientefeign/validar/cliente/ClienteCheckClient.java
+++ clientefeign/src/main/java/pe/com/nttdata/clientefeign/validar/cliente/ClienteCheckClient.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //para el modulo de validarCliente
-@FeignClient("validarcliente")// viene del nombre del service del properties
+//@FeignClient("validarcliente")// viene del nombre del service del properties
+@FeignClient(
+        name = "validarcliente",
+        url = "${clientefeign.validarcliente.url}"
+)
 public interface ClienteCheckClient {
     @GetMapping(path = "api/v1/cliente-check/{clienteId}")//viene del la uri del controller
     ClienteCheckResponse validarCliente(@PathVariable("clienteId") Integer clienteId);// viene del check controller
