5229b9b924b8809688a1dd7a1edad66b9ffc2f72
freddyfernandez <fernandezyc@hotmail.com>
2022-08-17 18:08:30 -0500

    config server v0.2

--- clientefeign/src/main/java/pe/com/nttdata/Main.java
+++
@@ -1,7 +0,0 @@
-package pe.com.nttdata;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- validarCliente/src/main/java/pe/com/nttdata/validar/cliente/ClienteCheckApplication.java
+++ validarCliente/src/main/java/pe/com/nttdata/validar/cliente/ClienteCheckApplication.java
@@ -2,8 +2,13 @@ package pe.com.nttdata.validar.cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+//referenciando a el modulo tipo libreria
+@EnableFeignClients(
+        basePackages = "pe.com.nttdata.clientefeign"
+)
 public class ClienteCheckApplication {
     public static void main(String[] args) {
 
